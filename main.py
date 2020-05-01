bef4edc1e697db77430f48be93516e8130450535
jawello <jawello@gmail.com>
2020-05-01 22:24:56 +0300

    Create simple app

---
+++ main.py
@@ -0,0 +1,59 @@
+import yaml
+from aiohttp import web
+from aiohttp.web import Application, HTTPInternalServerError
+from aiohttp.web_response import Response
+import json
+
+import logging
+
+log = logging.getLogger(__name__)
+routes = web.RouteTableDef()
+
+
+async def init_app(config) -> Application:
+    app = web.Application()
+    app['config'] = config
+    app.add_routes(routes)
+    log.debug(app['config'])
+    return app
+
+
+def load_config(config_file):
+    with open(config_file, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+@routes.get("/health")
+async def health_get(request) -> Response:
+    try:
+        return Response(body=json.dumps({"status": "OK"}), headers={'content-type': 'application/json'})
+    except Exception as ex:
+        log.warning(f"Endpoint: health, Method: get. Error:{str(ex)}")
+        return HTTPInternalServerError()
+
+
+def main(config_path):
+    config = load_config(config_path)
+    logging.basicConfig(level=logging.DEBUG)
+    app = init_app(config)
+    app_config = config.get('app', None)
+    if not app_config:
+        web.run_app(app, port=app_config.get('port', 8000))
+    else:
+        web.run_app(app, port=8000)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="Provide path to config file")
+    args = parser.parse_args()
+
+    if args.config:
+        main(args.config)
+    else:
+        parser.print_help()
