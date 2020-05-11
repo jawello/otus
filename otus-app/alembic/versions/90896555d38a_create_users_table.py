6f039704c1bb68a1fc774aace86890733dd6f7c3
jawello <jawello@gmail.com>
2020-05-11 22:19:46 +0300

    Add alembic

---
+++ otus-app/alembic/versions/90896555d38a_create_users_table.py
@@ -0,0 +1,29 @@
+"""create users table
+
+Revision ID: 90896555d38a
+Revises: 
+Create Date: 2020-05-11 22:09:43.368399
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '90896555d38a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('users',
+                    sa.Column('id', sa.Integer, primary_key=True),
+                    sa.Column('name', sa.String, nullable=False),
+                    sa.Column('login', sa.String, nullable=False, unique=True),
+                    sa.Column('password', sa.String, nullable=False)
+                    )
+
+
+def downgrade():
+    op.drop_table('users')
